c571c56520b5f66510da5f2d0c9f7c12e347bee8
jungryuel <zldrktduf0927@naver.com>
2024-05-21 11:49:18 +0900

    ci test

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.devops;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DevopsApplicationTests {
@@ -10,4 +13,12 @@ class DevopsApplicationTests {
 	void contextLoads() {
 	}
 
+	@Value("${spring.datasource.username}")
+	private String name;
+
+	@Test
+	void dbUrlIsCorrect() {
+		String userName = "sa";
+		assertEquals(userName, name);
+	}
 }
